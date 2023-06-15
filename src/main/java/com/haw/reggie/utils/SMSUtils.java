94ee886398d8a211354e0d9e1843a71f1adcfe58
whawha <65757396+wha-cloud@users.noreply.github.com>
2023-06-15 16:29:41 +0800

    Update SMSUtils.java

--- src/main/java/com/haw/reggie/utils/SMSUtils.java
+++ src/main/java/com/haw/reggie/utils/SMSUtils.java
@@ -20,7 +20,7 @@ public class SMSUtils {
 	 * @param param 参数
 	 */
 	public static void sendMessage(String signName, String templateCode,String phoneNumbers,String param){
-		DefaultProfile profile = DefaultProfile.getProfile("cn-hangzhou", "LTAI5tPK6vxKb61DoNv7XLGq", "0rrWAGpqX10OVkPvPtwlryJhJ1qFso");
+		DefaultProfile profile = DefaultProfile.getProfile("cn-hangzhou", "", "");
 		IAcsClient client = new DefaultAcsClient(profile);
 
 		SendSmsRequest request = new SendSmsRequest();
